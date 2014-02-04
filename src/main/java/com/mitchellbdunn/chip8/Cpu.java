642a4d8fbf38077eb55bdfc8a03d01465625e044
Mitch <mitchell.brant.dunn@gmail.com>
2014-02-03 19:40:58 -0500

    Updated opcodes
    Added javadocs explaining each opcode.  Wrote some of the smaller
    opcodes.

--- src/main/java/com/mitchellbdunn/chip8/Cpu.java
+++ src/main/java/com/mitchellbdunn/chip8/Cpu.java
@@ -1,5 +1,6 @@
 package com.mitchellbdunn.chip8;
 
+import java.util.Random;
 import java.util.Stack;
 
 /**
@@ -14,9 +15,11 @@ public class Cpu {
     private int delayTimer;
     private int soundTimer;
     private int programCounter;
+    private Random rng;
 
     public Cpu() {
         initializeCpu();
+        rng = new Random();
     }
 
     public final void initializeCpu() {
@@ -32,11 +35,18 @@ public class Cpu {
     }
 
     public void runOpcode(int opcode) {
+        // Get the possible variables needed from the opcode
         int nnn = Chip8Util.getNNN(opcode);
         int nn = Chip8Util.getNN(opcode);
         int n = Chip8Util.getN(opcode);
         int x = Chip8Util.getX(opcode);
         int y = Chip8Util.getY(opcode);
+        
+        // Increase the program counter; there are some opcodes where
+        // this won't matter since we set the program counter there
+        incrementProgramCounter();
+        
+        // Run the appropriate opcode
         switch (opcode & 0xF000) {
             case 0x0000:
                 switch (opcode & 0x000F) {
@@ -159,140 +169,323 @@ public class Cpu {
         }
     }
 
+    /**
+     * Clears the screen.
+     */
     private void opcode00E0() {
 
     }
 
+    /**
+     * Returns from a subroutine.
+     */
     private void opcode00EE() {
-
+        programCounter = stack.pop();
     }
 
+    /**
+     * Jumps to address NNN.
+     * @param nnn address to set program counter to.
+     */
     private void opcode1NNN(int nnn) {
-
+        programCounter = nnn;
     }
 
+    /**
+     * Calls subroutine at NNN.
+     * @param nnn address of start of subroutine.
+     */
     private void opcode2NNN(int nnn) {
-
+        stack.push(programCounter);
+        programCounter = nnn;
     }
 
+    /**
+     * Skips the next instruction if VX equals NN.
+     * @param x register to check for equality.
+     * @param nn value to check if equal to register VX.
+     */
     private void opcode3XNN(int x, int nn) {
-
+        if (registerV[x] == nn) {
+            incrementProgramCounter();
+        }
     }
 
+    /**
+     * Skips the next instruction if VX doesn't equal NN.
+     * @param x register to check for equality.
+     * @param nn value to check if not equal to register VX.
+     */
     private void opcode4XNN(int x, int nn) {
-
+        if (registerV[x] != nn) {
+            incrementProgramCounter();
+        }
     }
 
+    /**
+     * Skips the next instruction if VX equals VY.
+     * @param x register to check for equality.
+     * @param y register to check for equality.
+     */
     private void opcode5XY0(int x, int y) {
-
+        if (registerV[x] == registerV[y]) {
+            incrementProgramCounter();
+        }
     }
 
+    /**
+     * Sets VX to NN.
+     * @param x register to set value.
+     * @param nn value to set for register VX.
+     */
     private void opcode6XNN(int x, int nn) {
-
+        registerV[x] = nn;
     }
 
+    /**
+     * Adds NN to VX.
+     * @param x register to add value to.
+     * @param nn value to add to register VX.
+     */
     private void opcode7XNN(int x, int nn) {
-
+        registerV[x] += nn;
     }
 
+    /**
+     * Sets VX to the value of VY.
+     * @param x register to set value.
+     * @param y register to get the value to set in register VX.
+     */
     private void opcode8XY0(int x, int y) {
-
+        registerV[x] = registerV[y];
     }
 
+    /**
+     * Sets VX to VX or VY.
+     * @param x register to set value.
+     * @param y register to perform bitwise OR with register VX.
+     */
     private void opcode8XY1(int x, int y) {
-
+        registerV[x] = registerV[x] | registerV[y];
     }
 
+    /**
+     * Sets VX to VX and VY.
+     * @param x register to set value.
+     * @param y register to perform bitwise AND with register VX.
+     */
     private void opcode8XY2(int x, int y) {
-
+        registerV[x] = registerV[x] & registerV[y];
     }
 
+    /**
+     * Sets VX to VX xor VY.
+     * @param x register to set value.
+     * @param y register to perform bitwise XOR with register VX.
+     */
     private void opcode8XY3(int x, int y) {
-
+        registerV[x] = registerV[x] ^ registerV[y];
     }
 
+    /**
+     * Adds VY to VX.  VF is set to 1 when there's a carry, and to 0 when
+     * there isn't.
+     * @param x register to add value to.
+     * @param y register to get value and to add to register VX.
+     */
     private void opcode8XY4(int x, int y) {
 
     }
 
+    /**
+     * VY is subtracted from VX.  VF is set to 0 when there's a borrow, and
+     * 1 when there isn't.
+     * @param x register to subtract value from.
+     * @param y register to get value to subtract from register VX.
+     */
     private void opcode8XY5(int x, int y) {
 
     }
 
+    /**
+     * Shifts VX right by one.  VF is set to the value of the least significant
+     * bit of VX before the shift.
+     * @param x register to shift bits right by one.
+     * @param y not used.
+     */
     private void opcode8XY6(int x, int y) {
 
     }
 
+    /**
+     * Sets VX to VY minus VX.  VF is set to 0 when there's a borrow, and 1 when
+     * there isn't.
+     * @param x register to set value of subtraction to as well as get value of
+     * subtrahend.
+     * @param y register to get value of minuend for subtraction.
+     */
     private void opcode8XY7(int x, int y) {
 
     }
 
+    /**
+     * Shifts VX left by one.  VF is set to the value of the most significant
+     * bit of VX before the shift.
+     * @param x register to shift bits left by one.
+     * @param y not used.
+     */
     private void opcode8XYE(int x, int y) {
 
     }
 
+    /**
+     * Skips the next instruction if VX doesn't equal VY.
+     * @param x register to check for equality.
+     * @param y register to check for equality.
+     */
     private void opcode9XY0(int x, int y) {
-
+        if (registerV[x] != registerV[y]) {
+            incrementProgramCounter();
+        }
     }
 
+    /**
+     * Sets I to the address NNN.
+     * @param nnn value to set for I.
+     */
     private void opcodeANNN(int nnn) {
-
+        registerI = nnn;
     }
 
+    /**
+     * Jumps to the address NNN plus V0.
+     * @param nnn value to jump to plus value of register V0.
+     */
     private void opcodeBNNN(int nnn) {
-
+        programCounter = nnn + registerV[0];
     }
 
+    /**
+     * Sets VX to a random number and NN.
+     * @param x register to set value.
+     * @param nn value to get bitwise AND with a random number for register VX.
+     */
     private void opcodeCXNN(int x, int nn) {
-
+        registerV[x] = rng.nextInt() & nn;
     }
 
+    /**
+     * Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a
+     * height of N pixels.  Each row of 8 pixels is read as bit-coded (with the
+     * most significant bit of each byte displayed on the left) starting from
+     * memory location I; I value doesn't change after the execution of this
+     * instruction.  As described above, VF is set to 1 if any screen pixels are
+     * flipped from set to unset when the sprite is drawn, and to 0 if that
+     * doesn't happen.
+     * @param x register that holds X location to draw sprite.
+     * @param y register that holds Y location to draw sprite.
+     * @param n height of the sprite
+     */
     private void opcodeDXYN(int x, int y, int n) {
 
     }
 
+    /**
+     * Skips the next instruction if the key stored in VX is pressed.
+     * @param x register that holds a value that will map to a key
+     */
     private void opcodeEX9E(int x) {
 
     }
 
+    /**
+     * Skips the next instruction if the key stored in VX isn't pressed.
+     * @param x register that holds a value that will map to a key
+     */
     private void opcodeEXA1(int x) {
 
     }
 
+    /**
+     * Sets VX to the value of the delay timer.
+     * @param x register to set to value of delay timer.
+     */
     private void opcodeFX07(int x) {
-
+        registerV[x] = delayTimer;
     }
 
+    /**
+     * A key press is awaited, and then stored in VX.
+     * @param x register to store which key is pressed.
+     */
     private void opcodeFX0A(int x) {
 
     }
 
+    /**
+     * Sets the delay timer to VX.
+     * @param x register to set with the value of the delay timer.
+     */
     private void opcodeFX15(int x) {
-
+        delayTimer = registerV[x];
     }
 
+    /**
+     * Sets the sound timer to VX.
+     * @param x register to set with the value of the sound timer.
+     */
     private void opcodeFX18(int x) {
-
+        soundTimer = registerV[x];
     }
 
+    /**
+     * Adds VX to I.
+     * @param x register to add to I.
+     */
     private void opcodeFX1E(int x) {
-
+        registerI += registerV[x];
     }
 
+    /**
+     * Sets I to the location of the sprite for the character in VX. Characters 
+     * 0-F (in hexadecimal) are represented by a 4x5 font.
+     * @param x register to get value of the sprite for the character.
+     */
     private void opcodeFX29(int x) {
 
     }
 
+    /**
+     * Stores the Binary-coded decimal representation of VX, with the most 
+     * significant of three digits at the address in I, the middle digit at I 
+     * plus 1, and the least significant digit at I plus 2. (In other words, 
+     * take the decimal representation of VX, place the hundreds digit in memory 
+     * at location in I, the tens digit at location I+1, and the ones digit at 
+     * location I+2.)
+     * @param x register to get decimal value and store the binary-coded
+     * representation.
+     */
     private void opcodeFX33(int x) {
 
     }
 
+    /**
+     * Stores V0 to VX in memory starting at address I.
+     * @param x last register to store in memory.
+     */
     private void opcodeFX55(int x) {
 
     }
 
+    /**
+     * Fills V0 to VX with values from memory starting at address I.
+     * @param x last register to store from memory.
+     */
     private void opcodeFX65(int x) {
 
     }
 
+    private void incrementProgramCounter() {
+        programCounter = programCounter + 2;
+    }
 }
